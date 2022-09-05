9e5fda59e0cacf525fd145ee4a24d0f2a72ac9c0
HeadsetRuler <alph4numb3r@gmail.com>
2022-09-05 11:19:06 +0200

    Finished porting to CSharp

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+  enum DaySegment : byte { Night, Morning, Afternoon, Evening }
+  private static int Main(string[] args)
+  {    
+    DateTime timestamp;
+    if (args.Length > 0) 
+    {
+      if (!DateTime.TryParse(args[0], out timestamp)){
+        Console.WriteLine("Invalid timestamp given: {0}", args[0]);
+        return 1;
+      }
+    }else{
+      timestamp = DateTime.Now;
+    }
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Good {0}! It is currently {1}", GetDaySegment(timestamp), timestamp.ToShortTimeString());
+    return 0;
+  }
+  /// <summary>
+  /// Gets the day segment of the given timestamp, defaulting to now if no timestamp is given.
+  /// </summary>
+  /// <param name="timestamp">The timestamp to get the day segment for. Defaults to now if not specified.</param>
+  private static DaySegment GetDaySegment(DateTime? timestamp = null) {
+    timestamp ??= DateTime.Now;
+    return (DaySegment) (timestamp.Value.Hour / 6);
+  }
+}
